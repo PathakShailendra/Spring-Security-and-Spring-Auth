d14ada7ebb470f94faa0992f3e8be23720f22cf6
Shailendra Pathak <pathakharsh813@gmail.com>
2025-07-29 12:13:17 +0530

    Spring Security and Hashing Completed

---
+++ src/main/java/com/sec/securitydemo/GreetingsController.java
@@ -0,0 +1,27 @@
+package com.sec.securitydemo;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint() {
+        return "Hello User";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String asminEndPoint() {
+        return "Hello admin";
+    }
+}
